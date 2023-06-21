d1d2b1f06bf053a7aa8e9acad1cf1db30c43c193
loicmaes <97891718+loicmaes@users.noreply.github.com>
2023-06-21 08:25:16 +0200

    :rocket: Plugin bases

---
+++ src/main/java/fr/maesloic/library/starterkit/paper/PaperPlugin.java
@@ -0,0 +1,6 @@
+package fr.maesloic.library.starterkit.paper;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class PaperPlugin extends JavaPlugin {
+}
---
+++ src/main/java/fr/maesloic/library/starterkit/waterfall/WaterfallPlugin.java
@@ -0,0 +1,6 @@
+package fr.maesloic.library.starterkit.waterfall;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public class WaterfallPlugin extends Plugin {
+}
